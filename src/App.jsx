278a338f5269d31c8b683ea08d543e0f742dc1e5
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-11-30 15:33:55 -0300

    footer inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   )
 }
