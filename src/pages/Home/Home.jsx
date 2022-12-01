179e2bdc2d6b1a2b630e719cb930a557df95977f
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-12-01 11:07:27 -0300

    home inicial

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
 import style from './Home.module.css';
 
 export default function Home() {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products/')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setProducts(data)
+            })
+    }, [])
+
     return (
-        <h1>Home</h1>
+        <main>
+            <div>
+                
+            </div>
+            <ul className={style.products_list}>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} alt="Product image" />
+                        <p>{product.title}</p>
+                        <p>{product.price}</p>
+                    </li>
+                ))}
+            </ul>
+        </main>
     )
 }
